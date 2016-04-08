ccde44b04129fbcd65dd6a5661e97cdd9eb6b978
Paul Lee <paulyjlee@gmail.com>
2016-04-07 20:42:01 -0400

    TODO 1

--- assignment4/js/script.js
+++ assignment4/js/script.js
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  
+  console.log("categories", categories);
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
@@ -101,7 +101,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+      console.log("chosenCategoryShortName", chosenCategoryShortName);
       
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,7 +116,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       // 
-      // var homeHtmlToInsertIntoMainPage = ....
+      // var homeHtmlToInsertIntoMainPage = 
+      // console.log("homeHtmlToInsertIntoMainPage", homeHtmlToInsertIntoMainPage)
       
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
@@ -130,6 +132,8 @@ function buildAndShowHomeHTML (categories) {
 
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
+  console.log("invoked chooseRandomCategory");
+  
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
